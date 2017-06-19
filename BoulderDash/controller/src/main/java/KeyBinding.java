01402148f45fa9d7114cf4d5d92f1e9180017c29
wukzu <loick.legay@orange.fr>
2017-06-19 10:54:34 +0200

    KeyBinding

---
+++ BoulderDash/controller/src/main/java/KeyBinding.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ *
+ * Key event
+ * @author      Loick Legay
+ * @since       2017-06-19
+ * 
+ */
+public class KeyBinding {
+	private int id;
+
+	
+	public void keyPressed (int id) {
+		
+	}
+	public void keyReleased (int id) {
+		
+	}
+	public void keyType (int id) {
+		
+	}
+	
+	
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+}
