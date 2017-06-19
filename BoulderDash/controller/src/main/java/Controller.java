8b50c22bc24e68ea8f66b4bdd198df93b4062fd8
wukzu <loick.legay@orange.fr>
2017-06-19 10:51:01 +0200

    test

--- BoulderDash/controller/src/main/java/Controller.java
+++ BoulderDash/controller/src/main/java/Controller.java
@@ -1,4 +1,44 @@
+import javax.swing.text.View;
 
 public class Controller {
-	//test
+	private int level;
+	private char[] map;
+
+	/**
+	 * 
+	 * @author Loick Legay
+	 * @since 2017-06-19
+	 * 
+	 *
+	 */
+	
+	
+	public void start(int level) {
+		
+	}
+	
+	
+	
+	public char initMap(int level) {
+		
+		
+		return 0;
+	}
+	
+
+	
+	public void inGame() {
+		
+	}
+	
+	
+	//getter
+	public int getLevel() {
+		return level;
+	}
+
+	//setter
+	public void setLevel(int level) {
+		this.level = level;
+	}
 }
