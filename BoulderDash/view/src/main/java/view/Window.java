771973cadc945d3e1e7345c05a21ebde2227b206
Amaknes <amaury.vincent@viacesi.fr>
2017-06-19 10:05:06 +0200

    More test of Javadoc

--- BoulderDash/view/src/main/java/view/Window.java
+++ BoulderDash/view/src/main/java/view/Window.java
@@ -5,15 +5,26 @@ package view;
  * Spawns the frame and the panel and uses them in a single Class
  * @author      Amaury Vincent
  * @since       2017-06-16
+ * 
  */
 public class Window 
 {
 	Frame WindowFrame;
 	Panel WindowPanel;
-
+	
 	Window() 
 	{
 		this.WindowFrame = new Frame();
 		this.WindowPanel = new Panel();
 	}
+	/**
+	 * Displays the actual state of the Game
+     * @author      Amaury Vincent
+	 *
+	 */
+	void Display()
+	{
+		
+	}
 }
+
