2d46a716a9d92d30e2f33707940474a98a4b5863
Amaknes <amaury.vincent@viacesi.fr>
2017-06-22 01:27:17 +0200

    Tumbling Done and beginning of Simple Gravity

--- BoulderDash/controller/src/main/java/controller/StrategyDecider.java
+++ BoulderDash/controller/src/main/java/controller/StrategyDecider.java
@@ -14,26 +14,48 @@ public class StrategyDecider {
 	char TestedCase1;
 	if(TestedCase == 'G' || TestedCase == 'R') 
 	{
-		//DébutCheckTumbling
+		//BeginCheckTumbling
 		TestedCase = controller.getTableauValue(CaseX,CaseY-1);
 		if(TestedCase == 'G' || TestedCase == 'R')
 		{
-		 //DébutCheckTumblingLeft
 		 TestedCase = controller.getTableauValue(CaseX-1,CaseY);
 		 TestedCase1 = controller.getTableauValue(CaseX-1,CaseY-1);
 		 if(TestedCase == 'V' && TestedCase1 == 'V')
 		 	{
-			 controller.setTableauValue(CaseX, CaseY, 'V');
-			 if(TestedCaseOrigin == 'D')
+			 	controller.setTableauValue(CaseX, CaseY, 'V');
+			 	if(TestedCaseOrigin == 'D')
+			 		{
+			 		this.ChangeStrategy(new Tumbling(true,'G'));
+			 		}
+				else
+			 		{
+					this.ChangeStrategy(new Tumbling(true,'R'));
+			 		}
+		 	}
+		 else
+		 	{
+			 TestedCase = controller.getTableauValue(CaseX+1,CaseY);
+			 TestedCase1 = controller.getTableauValue(CaseX+1,CaseY-1);
+			 if(TestedCase == 'V' && TestedCase1 == 'V')
 			 	{
-				 
-			 	}
-			 else
-			 	{
-				 
+				 	controller.setTableauValue(CaseX, CaseY, 'V');
+				 	if(TestedCaseOrigin == 'D')
+				 		{
+				 		this.ChangeStrategy(new Tumbling(false,'G'));
+				 		}
+					else
+				 		{
+						this.ChangeStrategy(new Tumbling(false,'R'));
+				 		}
 			 	}
 		 	}
+		 //End Tumbling Test
 		}
-		}
+		//Begin the Simple System-Gravity checks
+		 if(TestedCase == 'V')
+		 {
+			 
+		 }
+	}
  }
 }
