d981c93ce12947c4f29238c65f7bb7baa485fd4c
wukzu <loick.legay@orange.fr>
2017-06-21 16:38:50 +0200

    Move left (1sec)

--- BoulderDash/view/src/main/java/entities/Player.java
+++ BoulderDash/view/src/main/java/entities/Player.java
@@ -20,26 +20,31 @@ public class Player extends Entities {
 	}
 	
 	public static void move(int a, char[][] tabMap){
-		/*
-	if(a==1) { //left
 		
-		for(int i = 0; i < 25; i++) {
-            for(int j =0; j < 25; j++) {
-                 
-            	switch(tabMap[i][j]) {
-            	case 'P':
-            		tabMap[i][j] = 'V';
-            		tabMap[i][j+1] = 'P';
-            		if(tabMap[i][j+1]=='W') {
-            			break;
-            		}
-            		
-            	}
-             
-            	System.out.print(tabMap[i][j]);		
-            }
-            System.out.println("");
-        }
-	}*/
+		if(a==1) { //left
+			int found=0;
+			for(int i = 0; i < 25; i++) {
+	            for(int j =0; j < 25; j++) {
+	                 
+	            	if(tabMap[i][j]=='P' && found==0) {
+	            		if(tabMap[i][j+1]=='W') {
+	            			found=1;
+	            			break;
+	            		}
+	            		tabMap[i][j] = 'V';
+	            		tabMap[i][j+1] = 'P';
+	            		found=1;
+	            		break;
+	            	}
+	            }
+	        }
+			for(int i = 0; i < 25; i++) {
+	            for(int j =0; j < 25; j++) {
+	            	System.out.print(tabMap[i][j]);		
+	            }
+	            System.out.println("");
+	        }
+	        System.out.println("");
+		}
 	}
 }
