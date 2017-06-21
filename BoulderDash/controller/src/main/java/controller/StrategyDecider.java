41fcf355c2e5172845dcb9722bafdc1f13bba8d5
Amaknes <amaury.vincent@viacesi.fr>
2017-06-21 16:47:30 +0200

    Strategy Decider update on structure

--- BoulderDash/controller/src/main/java/controller/StrategyDecider.java
+++ BoulderDash/controller/src/main/java/controller/StrategyDecider.java
@@ -1,18 +1,28 @@
 package controller;
 
-
 public class StrategyDecider {
- private Strategy Strat;
- public void ChangeStrategy(Strategy NewStrat)
- {
-	 this.Strat= NewStrat;
- }
- public void DefineStrategyFromContext(int CaseX,int CaseY, Controller controller)
+	private Strategy Strat;
+
+	public void ChangeStrategy(Strategy NewStrat) {
+		this.Strat = NewStrat;
+	}
+
+	public void DefineStrategyFromContext(int CaseX,int CaseY, Controller controller)
  {
 	char TestedCase = controller.getTableauValue(CaseX,CaseY);
+	char TestedCase1;
 	if(TestedCase == 'G' || TestedCase == 'R') 
 	{
-		
-	}
+		TestedCase = controller.getTableauValue(CaseX,CaseY-1);
+		if(TestedCase == 'G' || TestedCase == 'R')
+		{
+		 TestedCase = controller.getTableauValue(CaseX,CaseY);
+		 TestedCase1 = controller.getTableauValue(CaseX,CaseY);
+		 if(TestedCase == 'V' && TestedCase1 == 'V')
+		 	{
+			 
+		 	}
+		}
+		}
  }
 }
