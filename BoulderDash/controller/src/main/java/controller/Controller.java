459e425b462872d28e702e6f3234b6764388546a
Amaknes <amaury.vincent@viacesi.fr>
2017-06-21 09:35:41 +0200

    Getter and Setter for Table Values

--- BoulderDash/controller/src/main/java/controller/Controller.java
+++ BoulderDash/controller/src/main/java/controller/Controller.java
@@ -2,64 +2,58 @@ package controller;
 
 import javax.swing.text.View;
 
-
 import model.IModel;
 import view.IView;
-import view.Window;
+
 /**
  * Controller
- * @author      Loick Legay 
- * @since       2017-06-20
+ * 
+ * @author Loick Legay
+ * @since 2017-06-20
  * 
  */
 public class Controller {
 	private int level;
 	private char[][] map;
 	private boolean gameRunning;
-	Window window;
-	
+
 	/** The view. */
 	private final IView view;
 
 	/** The model. */
 	private final IModel model;
-		
-		
 
-	public Controller (int level) {
-		this.view=null;
-		this.model=null;
+	public Controller(int level) {
+		this.view = null;
+		this.model = null;
 		this.initMap(level);
 		gameRunning = true;
 		this.level = level;
-		this.window= new Window();
-		
 	}
 
-
 	public void start() {
-		
-		
-		
+
 	}
 
 	public char initMap(int level) {
 
 		return 0;
 	}
+
 	/**
 	 * Controller
-	 * @author      Amaury VINCENT 
-	 * @since       2017-06-20
+	 * 
+	 * @author Amaury VINCENT
+	 * @since 2017-06-20
 	 * 
 	 */
 	public void inGameLoop() {
-		//RockfordDeplacementAcknoledgement
-		
-		//Monster Deplacement
-		
-		//Gravity
-		
+		// RockfordDeplacementAcknoledgement
+
+		// Monster Deplacement
+
+		// Gravity
+
 	}
 
 	// getter
@@ -70,5 +64,13 @@ public class Controller {
 	// setter
 	public void setLevel(int level) {
 		this.level = level;
+	}
+
+	public char getTableauValue(int CaseX,int CaseY) {
+		return this.map[CaseX][CaseY];
+	}
+
+	public void setTableauValue(int CaseX, int CaseY, char Value) {
+		this.map[CaseX][CaseY] = Value;
 	}
 }
